4ea2d454ade1de9b64748e107338050c5b2928a0
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2023-03-10 17:34:14 +0530

    basic JS config's

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "@/components/chat";
 import "./App.css";
 
 function App() {
-  return <div className="app">Chat GPT App</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Chat />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
